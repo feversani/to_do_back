95615e671ad73d732d468a28da51909850612da1
fvrsn <diego.feversani@gmail.com>
2022-01-06 14:35:23 -0300

    Se agrego carpeta template y logica de renderizado HTML

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect, render_template
+
 
 app = Flask(__name__)
 
+todos = ['TODO','TODO2', 'TODO3']
+
 @app.route('/')
+def index():
+    user_ip = request.remote_addr
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip',user_ip)
+    
+    return response
+
+@app.route('/hello')
 def hello():
-    return("Hola Flask diego!")
+    user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'todos': todos
+    }
+    return render_template('hello.html', **context)
